8a549129798862d21529136f99f8d3fc923597b9
Om Gate <om@videodb.io>
2025-10-15 22:07:12 +0530

    feat: handle blocker changes

--- nodes/VideoDB/VideoDb.node.ts
+++ nodes/VideoDB/VideoDb.node.ts
@@ -6,6 +6,9 @@ import {
 	type INodeTypeDescription,
 	type ILoadOptionsFunctions,
 	type INodePropertyOptions,
+	NodeApiError,
+	JsonObject,
+	NodeOperationError,
 } from 'n8n-workflow';
 import { OPERATIONS, OPERATION_DETAILS, OPERATION_PARAMETERS } from './operations';
 
@@ -143,35 +146,53 @@ export class VideoDb implements INodeType {
 		const results: INodeExecutionData[] = [];
 
 		for (let i = 0; i < items.length; i++) {
-			const operationKey = this.getNodeParameter('operation', i) as string;
-			const operation = OPERATIONS[operationKey];
-			const params: Record<string, any> = {};
-			for (const param of operation.parameters) {
-				params[param.name] = this.getNodeParameter(param.name, i);
+			try {
+				const operationKey = this.getNodeParameter('operation', i) as string;
+				const operation = OPERATIONS[operationKey];
+				if (!operation) {
+					throw new NodeOperationError(this.getNode(), `Unknown operation: ${operationKey}`, {
+						itemIndex: i,
+					});
+				}
+				const params: Record<string, any> = {};
+				for (const param of operation.parameters) {
+					params[param.name] = this.getNodeParameter(param.name, i);
+				}
+				const credentials = await this.getCredentials('videoDBApi');
+				const baseUrl = credentials.baseUrl || 'https://api.videodb.io';
+				const endpoint =
+					typeof operation.endpoint === 'function'
+						? operation.endpoint(params)
+						: operation.endpoint;
+				const requestOptions: any = {
+					method: operation.method,
+					url: `${baseUrl}${endpoint}`,
+					headers: {
+						'x-access-token': credentials.apiKey!,
+					},
+				};
+				if (operation.method === 'GET') {
+					requestOptions.qs = operation.buildQuery(params);
+				} else {
+					requestOptions.body = operation.buildBody(params);
+					requestOptions.json = true;
+				}
+				const response = await this.helpers.httpRequestWithAuthentication.call(
+					this,
+					'videoDBApi',
+					requestOptions,
+				);
+				results.push({ json: response, pairedItem: { item: i } });
+			} catch (error) {
+				if (this.continueOnFail()) {
+					results.push({
+						json: error,
+						pairedItem: { item: i },
+					});
+					continue;
+				}
+				throw new NodeApiError(this.getNode(), error as JsonObject);
 			}
-			const credentials = await this.getCredentials('videoDBApi');
-			const baseUrl = credentials.baseUrl || 'https://api.videodb.io';
-			const endpoint =
-				typeof operation.endpoint === 'function' ? operation.endpoint(params) : operation.endpoint;
-			const requestOptions: any = {
-				method: operation.method,
-				url: `${baseUrl}${endpoint}`,
-				headers: {
-					'x-access-token': credentials.apiKey!,
-				},
-			};
-			if (operation.method === 'GET') {
-				requestOptions.qs = operation.buildQuery(params);
-			} else {
-				requestOptions.body = operation.buildBody(params);
-				requestOptions.json = true;
-			}
-			const response = await this.helpers.httpRequestWithAuthentication.call(
-				this,
-				'videoDBApi',
-				requestOptions,
-			);
-			results.push({ json: response });
 		}
 		return [results];
 	}
